23aab9468c7f478f1a23b91641767df48506731f
Michaël Hoste <michael.hoste@gmail.com>
2014-05-17 17:17:52 +0200

    next

--- lib/translation/flat_hash.rb
+++ lib/translation/flat_hash.rb
@@ -23,75 +23,98 @@ module Translation
         if !key_string or key_string == '' # if only one key like { 'en' => 'salut' }
           current_object[current_key] = value
         else
+          puts 'a'
           # Put back '[' if needed
           if key_string.count(']') > key_string.count('[')
+            puts 'b'
             key_string = '[' + key_string
           end
 
           while key_string != ''
+            puts 'c'
             # Next is array
             if key_string[0] == '['
+              puts 'd'
               array_pos = key_string.split(']', 2)[0]
               array_pos = array_pos.split('[', 2)[1].to_i
-
-              puts key_string
 
               key_string = key_string.split(']', 2).count == 2 ? key_string.split(']', 2)[1] : ""
               key_string = key_string[1..-1] if key_string[0] == '.'
 
               if current_object.is_a? Hash
+                puts 'e'
                 if !current_object.has_key?(current_key)
+                  puts 'f'
                   current_object[current_key] = []
                 end
                 current_object = current_object[current_key]
+                puts hash.inspect
+                puts current_object.inspect
                 current_key    = nil # next is array
               elsif current_object.is_a? Array
+                puts 'g'
+                puts hash.inspect
+                puts current_object.inspect
                 if !current_object[array_pos]
+                  puts "h - #{array_pos}"
                   current_object[array_pos] = []
+                  puts hash.inspect
+                  puts current_object.inspect
                 end
                 current_object = current_object[array_pos]
                 current_key    = nil # next is array
               end
 
-              puts current_object.inspect
-              puts '---'
-
               if key_string == ''
+                puts 'i'
                 current_object << value
               end
             # next is hash
             elsif key_string[0] != '[' && (key_string.include?('.') or key_string.include?('['))
+              puts 'j'
               new_key    = key_string.split(/\.|\[/, 2)[0]
               key_string = key_string.split(/\.|\[/, 2)[1]
 
               # Put back '[' if needed
               if key_string.count(']') > key_string.count('[')
+                puts 'k'
                 key_string = '[' + key_string
               end
 
               if current_object.is_a? Hash
+                puts 'l'
                 if !current_object.has_key?(current_key)
+                  puts 'm'
                   current_object[current_key] = {}
                 end
                 current_object = current_object[current_key]
                 current_key    = new_key
               elsif current_object.is_a? Array
+                puts 'n'
                 current_object << {}
                 current_object = current_object.last
                 current_key    = new_key
               end
             # next (last) is value
             else
+              puts 'o'
               new_key = key_string
 
               if current_object.is_a? Hash
+                puts 'p'
                 if !current_object.has_key?(current_key)
+                  puts 'q'
                   current_object[current_key] = {}
                 end
+                puts 'r'
                 current_object          = current_object[current_key]
                 current_object[new_key] = value
               elsif current_object.is_a? Array
+                puts 's'
+                puts hash.inspect
                 current_object << { key_string => value }
+                puts hash.inspect
+                puts '-------'
               end
               key_string = ''
             end
@@ -127,8 +150,8 @@ end
 #Translation::FlatHash.to_hash({ 'en.hello.salut' => 'Hello world' })
 #Translation::FlatHash.to_hash({ 'en.main.menu.stuff' => 'This is stuff' })
 #Translation::FlatHash.to_hash({ 'fr.salut' => 'blabla' })
-#Translation::FlatHash.to_hash({ 'fr[0]' => 'blabla' })
-#Translation::FlatHash.to_hash({ 'fr[0].bouh' => 'blabla' })
+#Translation::FlatHash.to_hash({ 'fr[0]' => 'blabla', })
+
 #Translation::FlatHash.to_hash({ 'fr[0].bouh.salut' => 'blabla' })
 #
 #Translation::FlatHash.to_hash({
@@ -137,5 +160,13 @@ end
 #  'fr.salut'           => 'blabla'
 #})
 
-Translation::FlatHash.to_hash({ 'en[0][0]' => 'hello',
-                                'en[0][1]' => 'world' })
+#Translation::FlatHash.to_hash({ 'en[0][0]' => 'hello',
+#                                'en[0][1]' => 'new',
+#                                'en[0][2]' => 'world',
+#                                'en[1][0]' => 'salut' })
+
+Translation::FlatHash.to_hash({ 'fr[0].bouh'  => 'blabla',
+                                'fr[1].hello' => 'blibli'})
+
+Translation::FlatHash.to_hash({ 'fr[0][0].bouh'  => 'blabla',
+                                'fr[0][1].hello' => 'blibli'})
