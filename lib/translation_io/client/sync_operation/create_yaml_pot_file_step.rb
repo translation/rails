3f4d21822186701b606538a9e63ba28aa6541fbc
Michaël Hoste <michael.hoste@gmail.com>
2014-08-31 17:25:10 +0200

    debug romain (undefined method start_with? for nil) in create_yaml_pot_file_step:22

--- lib/translation_io/client/sync_operation/create_yaml_pot_file_step.rb
+++ lib/translation_io/client/sync_operation/create_yaml_pot_file_step.rb
@@ -19,7 +19,8 @@ module TranslationIO
           end
 
           source_flat_string_tanslations = all_flat_translations.select do |key, value|
-            value.is_a?(String) && key.start_with?("#{@source_locale}.") && !key.start_with?("#{@source_locale}.faker.")
+            TranslationIO.info "#{key} - #{value}"
+            value.is_a?(String) && key.present? && key.start_with?("#{@source_locale}.") && !key.start_with?("#{@source_locale}.faker.")
           end
 
           pot_representation = GetText::PO.new
