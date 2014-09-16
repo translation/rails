45936902d1d0b55213b7925558df35913af621be
Aurélien Malisart <aurelien.malisart@gmail.com>
2014-09-16 11:01:58 +0200

    #91 desc rake

--- lib/translation_io/tasks.rb
+++ lib/translation_io/tasks.rb
@@ -1,15 +1,18 @@
 require 'gettext/tools'
 
 namespace :translation do
+
+  DESCRIPTIONS = { :init  => "Initialize your Translation.io project",
+    :sync  => "Send new translatable keys/strings and get new translations from Translation.io",
+    :purge => "Remove unused keys/strings from Translation.io using the current branch as reference"
+  }
+
   desc "Get configuration infos of Translation gem"
   task :config => :environment do
     puts TranslationIO.config
   end
 
-  { :init  => "Initialize your Translation.io project",
-    :sync  => "Send new translatable keys/strings and get new translations from Translation.io",
-    :purge => "Remove unused keys/strings from Translation.io using the current branch as reference"
-  }.each_pair do |t, description|
+  DESCRIPTIONS.each_pair do |t, description|
     desc description
     task t => :environment do
       if TranslationIO.client
