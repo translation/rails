51840574a245d2d04e58cfb1a24a8a2ed523626b
Michaël Hoste <michael.hoste@gmail.com>
2015-01-08 16:59:55 +0100

    better set_locale

--- lib/translation_io/controller.rb
+++ lib/translation_io/controller.rb
@@ -1,17 +1,27 @@
 module TranslationIO
   module Controller
     def set_locale
-      requested_locale = params[:locale]                     ||
-                         session[:locale]                    ||
-                         cookies[:locale]                    ||
-                         request.env['HTTP_ACCEPT_LANGUAGE'] ||
+      requested_locale = params[:locale]                                             ||
+                         session[:locale]                                            ||
+                         cookies[:locale]                                            ||
+                         extract_browser_locale(request.env['HTTP_ACCEPT_LANGUAGE']) ||
                          I18n.default_locale
 
       if I18n.available_locales.include?(requested_locale.to_sym)
         session[:locale] = requested_locale
         I18n.locale      = requested_locale
       else
-        redirect_to root_path(:locale => I18n.default_locale)
+        if respond_to?(:root_path)
+          redirect_to root_path(:locale => I18n.default_locale)
+        else
+          redirect_to "/?locale=#{I18n.default_locale}"
+        end
+      end
+    end
+
+    def extract_browser_locale(http_accept_language)
+      http_accept_language.to_s.scan(/[a-z]{2}(?:-[A-Z]{2})?/).detect do |candidate|
+        I18n.available_locales.include?(candidate.to_sym)
       end
     end
   end
