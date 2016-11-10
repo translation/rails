08d96ddc1a14149ed48fa721292cc0039e81d24a
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-11-10 14:24:16 +0100

    cleanups

--- lib/translation_io/content/config.rb
+++ lib/translation_io/content/config.rb
@@ -1,23 +1,17 @@
 module TranslationIO
   module Content
     class Config
-      attr_accessor :api_key, :locales_path, :yaml_locales_path
+      attr_accessor :api_key
       attr_accessor :source_locale, :target_locales
       attr_accessor :field_accessors
       attr_accessor :storage
       attr_accessor :endpoint
-      attr_accessor :verbose
-      attr_accessor :test
 
       def initialize
-        self.locales_path      = File.join('config', 'locales', 'gettext')
-        self.yaml_locales_path = File.join('config', 'locales')
-        self.source_locale     = :en
-        self.target_locales    = []
-        self.field_accessors   = true
-        self.endpoint          = 'https://translation.io/api'
-        self.verbose           = 1
-        self.test              = false
+        self.source_locale   = :en
+        self.target_locales  = []
+        self.field_accessors = true
+        self.endpoint        = 'https://translation.io/api'
 
         if defined?(Globalize)
           self.storage = :globalize
