f6ffaf4ca348a0b10e056e4cd3cd5bc8c3aecf93
Michaël Hoste <michael.hoste@gmail.com>
2016-12-15 14:22:44 +0100

    Error when unresolved conflicts in .translation_io (it can happen quite frequently and it breaks silently)

--- spec/translation/client/sync_operation/apply_yaml_source_edits_step_spec.rb
+++ spec/translation/client/sync_operation/apply_yaml_source_edits_step_spec.rb
@@ -1,8 +1,44 @@
 require 'spec_helper'
 
 describe TranslationIO::Client::SyncOperation::ApplyYamlSourceEditsStep do
-  it 'apply remote changes locally' do
+  it "doesn't accept a corrupted .translation_io file" do
+    yaml_locales_path = 'tmp/config/locales'
+    FileUtils.mkdir_p(yaml_locales_path)
 
+    File.open("#{yaml_locales_path}/.translation_io", 'w') do |file|
+      file.write <<EOS
+<<<<<<< HEAD
+timestamp: 1474639179
+=======
+timestamp: 1474629510
+>>>>>>> master
+EOS
+    end
+
+    source_locale   = 'en'
+    yaml_file_paths = Dir["#{yaml_locales_path}/*.yml"]
+
+    step_operation = TranslationIO::Client::SyncOperation::ApplyYamlSourceEditsStep.new(yaml_file_paths, source_locale)
+
+    step_operation.stub(:perform_source_edits_request) do
+      {
+        'project_name' => "whatever",
+        'project_url'  => "http://localhost:3000/somebody-that-i-used-to-know/whatever",
+        'source_edits' => [
+          {
+            'key'      => "main.hello",
+            'old_text' => "Hello world",
+            'new_text' => "Hello wonderful world"
+          }
+        ]
+      }
+    end
+
+    params = {}
+    expect { step_operation.run(params) }.to raise_error(SystemExit)
+  end
+
+  it 'apply remote changes locally' do
     yaml_locales_path = 'tmp/config/locales'
     FileUtils.mkdir_p(yaml_locales_path)
 
