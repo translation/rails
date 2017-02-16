80b9c8b20bd27f190d7df7de132d315d7f271df8
Michaël Hoste <michael.hoste@gmail.com>
2017-02-16 11:26:06 +0100

    If `ruby` is available, check the generated ruby files for syntax issues (DumpMarkupGettextKeysStep)

--- lib/translation_io/client/base_operation/dump_markup_gettext_keys_step.rb
+++ lib/translation_io/client/base_operation/dump_markup_gettext_keys_step.rb
@@ -21,11 +21,28 @@ module TranslationIO
                 file.puts "  #{entry}"
                 file.puts "end"
               end
+
+              # can happen sometimes if gettext parsing is wrong
+              if ruby_cmd_available?
+                remove_file_if_syntax_invalid(file_path, entry)
+              end
             end
           end
         end
 
         protected
+
+        def remove_file_if_syntax_invalid(file_path, entry)
+          if `ruby -c #{file_path} 2>/dev/null`.empty? # returns 'Syntax OK' if syntax valid
+            TranslationIO.info ""
+            TranslationIO.info "Warning - #{@markup_type.to_s.upcase} Gettext parsing failed: #{entry}"
+            TranslationIO.info "          This entry will be ignored until you fix it. Please note that"
+            TranslationIO.info "          this warning can sometimes be caused by complex interpolated strings."
+            TranslationIO.info ""
+
+            FileUtils.rm(file_path)
+          end
+        end
 
         def extracted_gettext_entries
           entries = []
@@ -40,6 +57,10 @@ module TranslationIO
           TranslationIO.info "#{entries.size} entries found", 2, 2
 
           entries
+        end
+
+        def ruby_cmd_available?
+          @ruby_cmd_available ||= `which ruby`.strip.length > 0
         end
       end
     end
