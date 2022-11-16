57808a0b370c6400b459d797a462574876cb1bf6
Michaël Hoste <michael.hoste@gmail.com>
2022-11-16 14:35:13 +0100

    Fix plural rule in source PO file by manually adding it (for GetText >= 3.3.9) (#53)
    * Manually manage plural rules for source PO file
    
    Manually manage plural rules for source PO file when GetText >= 3.3.9 and the introduction of optional `red-datasets` dependency for plural rules

--- lib/translation_io/client/base_operation/save_new_po_files_step.rb
+++ lib/translation_io/client/base_operation/save_new_po_files_step.rb
@@ -34,18 +34,26 @@ module TranslationIO
         # Create source locale PO file, with identical source and target
         # => Useful for correct fallbacks (cf. discussion https://github.com/translation/rails/issues/48)
         def create_source_po(text_domain)
-          source_locale = Locale::Tag.parse(@source_locale).to_s
+          source_locale = Locale::Tag.parse(@source_locale)
 
           pot_path = File.join(@locales_path, "#{text_domain}.pot")
-          po_path  = File.join(@locales_path, source_locale, "#{text_domain}.po")
+          po_path  = File.join(@locales_path, source_locale.to_s, "#{text_domain}.po")
 
           FileUtils.mkdir_p(File.dirname(po_path))
           FileUtils.rm(po_path) if File.exist?(po_path)
 
           # Generate source PO from POT and parse it
-          GetText::Tools::MsgInit.run('-i', pot_path, '-o', po_path, '-l', source_locale, '--no-translator')
+          GetText::Tools::MsgInit.run('-i', pot_path, '-o', po_path, '-l', source_locale.to_s, '--no-translator')
           po_entries = GetText::PO.new
           GetText::POParser.new.parse(File.read(po_path), po_entries)
+
+          # With GetText after 3.3.9 and the introduction of optional `red-datasets` dependency, we need to complete the plural forms manually
+          # Source languages must have only 2 plural forms, and only 2 different pluralization rules exist for these languages
+          if %w(ak am as bn fa fr gu hi hy kn ln mg nso pa si ti zu).include?(source_locale.language)
+            po_entries[''].msgstr.gsub!('Plural-Forms: nplurals=; plural=;', 'Plural-Forms: nplurals=2; plural=n > 1;')  # 0 and 1 are singular
+          else
+            po_entries[''].msgstr.gsub!('Plural-Forms: nplurals=; plural=;', 'Plural-Forms: nplurals=2; plural=n != 1;') # only 1 is singular
+          end
 
           # Fill with same target as source and save it
           po_entries.each do |po_entry|
